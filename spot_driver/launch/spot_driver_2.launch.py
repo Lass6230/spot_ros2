3d73277341bef1431e74569430e8eec72e838d46
lass6230 <due6230@gmail.com>
2023-10-18 16:37:11 +0200

    work with config file

--- spot_driver/launch/spot_driver_2.launch.py
+++ spot_driver/launch/spot_driver_2.launch.py
@@ -19,29 +19,29 @@ from launch_ros.substitutions import FindPackageShare
 THIS_PACKAGE = "spot_driver"
 
 
-def create_rviz_config(robot_name: str) -> None:
-    """Writes a configuration file for rviz to visualize a single spot robot"""
+# def create_rviz_config(robot_name: str) -> None:
+#     """Writes a configuration file for rviz to visualize a single spot robot"""
 
-    RVIZ_TEMPLATE_FILENAME = os.path.join(get_package_share_directory(THIS_PACKAGE), "rviz", "spot_template.yaml")
-    RVIZ_OUTPUT_FILENAME = os.path.join(get_package_share_directory(THIS_PACKAGE), "rviz", "spot.rviz")
+#     RVIZ_TEMPLATE_FILENAME = os.path.join(get_package_share_directory(THIS_PACKAGE), "rviz", "spot_template.yaml")
+#     RVIZ_OUTPUT_FILENAME = os.path.join(get_package_share_directory(THIS_PACKAGE), "rviz", "spot.rviz")
 
-    with open(RVIZ_TEMPLATE_FILENAME, "r") as template_file:
-        config = yaml.safe_load(template_file)
+#     with open(RVIZ_TEMPLATE_FILENAME, "r") as template_file:
+#         config = yaml.safe_load(template_file)
 
-        if robot_name:
-            # replace fixed frame with robot body frame
-            config["Visualization Manager"]["Global Options"]["Fixed Frame"] = f"{robot_name}/vision"
-            # Add robot models for each robot
-            for display in config["Visualization Manager"]["Displays"]:
-                if "RobotModel" in display["Class"]:
-                    display["Description Topic"]["Value"] = f"/{robot_name}/robot_description"
+#         if robot_name:
+#             # replace fixed frame with robot body frame
+#             config["Visualization Manager"]["Global Options"]["Fixed Frame"] = f"{robot_name}/vision"
+#             # Add robot models for each robot
+#             for display in config["Visualization Manager"]["Displays"]:
+#                 if "RobotModel" in display["Class"]:
+#                     display["Description Topic"]["Value"] = f"/{robot_name}/robot_description"
 
-                if "Image" in display["Class"]:
-                    topic_name = display["Topic"]["Value"]
-                    display["Topic"]["Value"] = f"/{robot_name}{topic_name}"
+#                 if "Image" in display["Class"]:
+#                     topic_name = display["Topic"]["Value"]
+#                     display["Topic"]["Value"] = f"/{robot_name}{topic_name}"
 
-    with open(RVIZ_OUTPUT_FILENAME, "w") as out_file:
-        yaml.dump(config, out_file)
+#     with open(RVIZ_OUTPUT_FILENAME, "w") as out_file:
+#         yaml.dump(config, out_file)
 
 
 def launch_setup(context: LaunchContext, ld: LaunchDescription) -> None:
@@ -98,33 +98,33 @@ def launch_setup(context: LaunchContext, ld: LaunchDescription) -> None:
     ld.add_action(robot_state_publisher)
 
     # It looks like passing an optional of value "None" gets converted to a string of value "None"
-    if not rviz_config_file or rviz_config_file == "None":
-        create_rviz_config(spot_name)
-        rviz_config_file = PathJoinSubstitution([FindPackageShare(THIS_PACKAGE), "rviz", "spot.rviz"]).perform(context)
+    # if not rviz_config_file or rviz_config_file == "None":
+    #     create_rviz_config(spot_name)
+    #     rviz_config_file = PathJoinSubstitution([FindPackageShare(THIS_PACKAGE), "rviz", "spot.rviz"]).perform(context)
 
-    rviz = launch_ros.actions.Node(
-        package="rviz2",
-        executable="rviz2",
-        name="rviz2",
-        arguments=["-d", rviz_config_file],
-        output="screen",
-        condition=IfCondition(launch_rviz),
-    )
+    # rviz = launch_ros.actions.Node(
+    #     package="rviz2",
+    #     executable="rviz2",
+    #     name="rviz2",
+    #     arguments=["-d", rviz_config_file],
+    #     output="screen",
+    #     condition=IfCondition(launch_rviz),
+    # )
 
-    ld.add_action(rviz)
+    # ld.add_action(rviz)
 
-    registered_depth_image_nodes = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            [
-                PathJoinSubstitution(
-                    [FindPackageShare(THIS_PACKAGE), "launch", "spot_depth_registered_publishers.launch.py"]
-                )
-            ]
-        ),
-        launch_arguments={"spot_name": spot_name, "has_arm": has_arm}.items(),
-        condition=IfCondition(use_depth_registered_nodelets),
-    )
-    ld.add_action(registered_depth_image_nodes)
+    # registered_depth_image_nodes = IncludeLaunchDescription(
+    #     PythonLaunchDescriptionSource(
+    #         [
+    #             PathJoinSubstitution(
+    #                 [FindPackageShare(THIS_PACKAGE), "launch", "spot_depth_registered_publishers.launch.py"]
+    #             )
+    #         ]
+    #     ),
+    #     launch_arguments={"spot_name": spot_name, "has_arm": has_arm}.items(),
+    #     condition=IfCondition(use_depth_registered_nodelets),
+    # )
+    # ld.add_action(registered_depth_image_nodes)
 
 
 def generate_launch_description() -> launch.LaunchDescription:
